94a44d7e33f8d5b7e02937f649366b258a49127b
Simone Taeggi <startae14@gmail.com>
2017-02-06 18:34:27 +0100

    Updating Scores and Changing the Active Player
    implementata logica per aggiornare lo score del turno e cambiare anche
    utente attivo

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ scores = [0,0];
 // punteggio del turno
 roundScore = 0;
 // utente attivo
-activePlayer = 1;
+activePlayer = 0;
 
 // nascondiamo il dado
 document.querySelector('.dice').style.display = 'none';
@@ -35,7 +35,27 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
   diceDOM.src = 'dice-' + dice + '.png';
   
   // 3. Aggiornare il ROUND SCORE se il numero che esce nn è 1
-  
+  // se il dado è diverso da 1
+  if(dice !== 1) {
+    // aggiungo il valore del dado al punteggio del turno
+    roundScore += dice;
+    // assegno al ele current del giocatore attivo il roundScore
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // passo all'altro giocatore
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    // risetto a zero i current score
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+    
+    // uso il metodo toggle per attivare o disattivare la classe active ai due giocatori
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    
+    // nascondo il dado
+    document.querySelector(".dice").style.display = "none";
+  }
   
   
 });
