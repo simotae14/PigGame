d64a49112c360da63b8cdb6e1048827f448da7ed
Simone Taeggi <startae14@gmail.com>
2017-02-07 10:50:18 +0100

    Finishing Touches: State Variables
    tocchi finali settata una var di stato

--- app.js
+++ app.js
@@ -8,51 +8,56 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 // seleziono il bottone per tirare dado
 document.querySelector('.btn-roll').addEventListener('click', function(){
-  // 1. Numero Random
-  var dice = Math.floor(Math.random() * 6) + 1;
-  // 2. Mostro il contenuto e la giusta immagine del dado
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  // cambio contenuto dell'attributo src del dado
-  diceDOM.src = 'dice-' + dice + '.png';
-  
-  // 3. Aggiornare il ROUND SCORE se il numero che esce nn è 1
-  // se il dado è diverso da 1
-  if(dice !== 1) {
-    // aggiungo il valore del dado al punteggio del turno
-    roundScore += dice;
-    // assegno al ele current del giocatore attivo il roundScore
-    document.querySelector("#current-" + activePlayer).textContent = roundScore;
-  } else {
-    // passo all'altro giocatore
-    nextPlayer();
+  if(gamePlaying){
+    // 1. Numero Random
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // 2. Mostro il contenuto e la giusta immagine del dado
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    // cambio contenuto dell'attributo src del dado
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Aggiornare il ROUND SCORE se il numero che esce nn è 1
+    // se il dado è diverso da 1
+    if(dice !== 1) {
+      // aggiungo il valore del dado al punteggio del turno
+      roundScore += dice;
+      // assegno al ele current del giocatore attivo il roundScore
+      document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    } else {
+      // passo all'altro giocatore
+      nextPlayer();
+    }    
   }
 });
 
 // Creo il listener al click del bottone hold per mantenere il punteggio
 document.querySelector('.btn-hold').addEventListener('click', function(){
-  // Aggiungere il CURRENT score a quello GLOBAL
-  scores[activePlayer] += roundScore; 
-  
-  // Aggiornare la UI
-  document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
-  
-  // Controllare se l'utente HA VINTO
-  if(scores[activePlayer] >= 100){
-    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
-    document.querySelector('.dice').style.display = 'none';
-    document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
-    document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
-    
-  } else {
-    // Prossimo giocatore
-    nextPlayer();    
+  if(gamePlaying){
+    // Aggiungere il CURRENT score a quello GLOBAL
+    scores[activePlayer] += roundScore; 
+
+    // Aggiornare la UI
+    document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
+
+    // Controllare se l'utente HA VINTO
+    if(scores[activePlayer] >= 100){
+      document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+      document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+      // setto la var di stato gamePlaying a false
+      gamePlaying = false;
+    } else {
+      // Prossimo giocatore
+      nextPlayer();    
+    }    
   }
 });
 
@@ -83,7 +88,9 @@ function init() {
   // punteggio del turno
   roundScore = 0;
   // utente attivo
-  activePlayer = 0;  
+  activePlayer = 0;
+  // var di stato gamePlaying
+  gamePlaying = true;
   // nascondiamo il dado
   document.querySelector('.dice').style.display = 'none';
   // setto tutti i valori degli score a 0
