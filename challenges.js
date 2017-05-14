8fb71d66575b18c78d79126b6b35621a2fca79a5
Simotae14 <startae14@gmail.com>
2017-05-14 14:53:21 +0200

    challenge 3 finished
    aggiunto un secondo dado con le regole delle sfide precedenti

--- challenges.js
+++ challenges.js
@@ -6,39 +6,41 @@ Change the game to follow these rules:
 
 2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good opportunity to use google to figure this out :)
 
-3. Add another dice to the game, so that there a re two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 // creo var del punteggio precedente del dado
-var lastDice;
+var lastDice1, lastDice2;
 
 // seleziono il bottone per tirare dado
 document.querySelector('.btn-roll').addEventListener('click', function(){
   if(gamePlaying){
     // 1. Numero Random
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
     // 2. Mostro il contenuto e la giusta immagine del dado
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
     // cambio contenuto dell'attributo src del dado
-    diceDOM.src = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
     // 3. Aggiornare il ROUND SCORE se il numero che esce nn è 1
     // se il dado è diverso da 1
     // se esce per due volte di fila il sei
-    if(dice === 6 && lastDice === 6) {
+    if((dice1 === 6 || dice2 == 0) && (lastDice1 === 6 || lastDice2 === 6)) {
       // Il giocatore perde lo score
       scores[activePlayer] = 0;
       // anche visivamente azzero lo score
       document.querySelector("#score-" + activePlayer).textContent = "0";
       // passo turno al prossimo giocatore
       nextPlayer();
-    } else if(dice !== 1) {
+    } else if(dice1 !== 1 && dice2 !== 1) {
       // aggiungo il valore del dado al punteggio del turno
-      roundScore += dice;
+      roundScore += dice1 + dice2;
       // assegno al ele current del giocatore attivo il roundScore
       document.querySelector("#current-" + activePlayer).textContent = roundScore;
     } else {
@@ -46,7 +48,8 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
       nextPlayer();
     }
     // aggiorno il valore di lastDice con quello di dice
-    lastDice = dice;
+    lastDice1 = dice1;
+    lastDice2 = dice2;
   }
 });
 
@@ -58,11 +61,26 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
     // Aggiornare la UI
     document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
-
+    
+    // recupero il valore del punteggio massimo
+    var input = document.querySelector(".final-score").value;
+    // creo il punteggio di vincita
+    var winningScore;
+    // se undefined, 0, null o "" viene COERCIZZATO a false
+    // altrimenti a true
+    if(input) {
+      // nel caso creo una var winningScore
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+    
+    
     // Controllare se l'utente HA VINTO
-    if(scores[activePlayer] >= 100){
+    if(scores[activePlayer] >= winningScore){
       document.querySelector("#name-" + activePlayer).textContent = "Winner!";
-      document.querySelector('.dice').style.display = 'none';
+      document.getElementById('dice-1').style.display = 'none';
+      document.getElementById('dice-2').style.display = 'none';
       document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
       document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
       // setto la var di stato gamePlaying a false
@@ -87,7 +105,8 @@ function nextPlayer() {
   document.querySelector(".player-1-panel").classList.toggle("active");
 
   // nascondo il dado
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 }
 
 // creo il listener sul bottone btn-new
@@ -105,7 +124,8 @@ function init() {
   // var di stato gamePlaying
   gamePlaying = true;
   // nascondiamo il dado
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
   // setto tutti i valori degli score a 0
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
